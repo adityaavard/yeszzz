60743d7f8b0b665ca98ccadf8ecb92d5273be3f3
Aditya Vardhan <adityagamar534@outlook.com>
2024-02-27 19:23:39 +0530

    loops

--- src/aditya/Main.java
+++ src/aditya/Main.java
@@ -1,32 +1,25 @@
 package aditya;
 import java.util.Scanner;
 
+import static java.sql.DriverManager.println;
+
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in); //scanner
+        // count till 100
 
-        System.out.println("program working!!!!!"); //"program working"
+        /*
+        System.out.println("1")
+        System.out.println("2")
+        System.out.println("3")
+        System.out.println("4")
 
-        System.out.println("javaW"); //"javaW"
+        //this is gonna take forever
+         */
 
-        System.out.println("enter decimel");
-
-        float number = scanner.nextFloat(); // activates input
-
-        System.out.println("yoar number is: " + number);
-
-        scanner.nextLine();
-
-        System.out.println("sealing of ur number: " + Math.ceil(number)); // ceiling
-        System.out.println("round of ur number: " + Math.round(number)); // round
-        System.out.println("floor of ur number: " + Math.floor(number)); // floor of decimal
-
-        System.out.println("type smth");
-
-        String ster = scanner.nextLine();
-
-        System.out.println("you entered: " + ster);
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
     }
 }
